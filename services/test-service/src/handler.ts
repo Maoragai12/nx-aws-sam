cf6c698ba628c1721baf818661ea42d8eec0cc47
Maor Agai <maoragai12@gmail.com>
2022-09-29 02:15:33 +0300

    Tests

--- services/test-service/src/handler.ts
+++ services/test-service/src/handler.ts
@@ -18,6 +18,6 @@ export const handler = (event: SQSEvent, context: Context) => {
     console.log('CONTEXT ==========> %j', context);
     console.log('DDDDD ==========> %j', context);
     console.log('CONTEXT ==========> %j', context);
-    console.log('CONTEXT ==========> %j', context);
+    console.log('GGGGG ==========> %j', context);
     console.log('CONTEXT ==========> %j', context);
 };
