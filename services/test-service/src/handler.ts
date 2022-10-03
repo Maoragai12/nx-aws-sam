10739dbaa78108252dee09a6f0e77f478ebde5d4
Maor Agai <maoragai12@gmail.com>
2022-10-03 16:18:42 +0300

    ssss

--- services/test-service/src/handler.ts
+++ services/test-service/src/handler.ts
@@ -20,6 +20,7 @@ export const handler = (event: SQSEvent, context: Context) => {
     console.log('GGGGG ==========> %j', context);
     console.log('GGGGG ==========> %j', context);
     console.log('GGGGG ==========> %j', context);
+    console.log('GGGGG ==========> %j', context);
     console.log('CONTEXT ==========> %j', context);
 
     const a = [ 1 ];
